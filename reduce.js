2af0b8abbfe6e82ae342cc7ffed5145297e9a3ea
Phillip <phv9495@saintjoe.edu>
2015-03-01 20:11:02 -0500

    I'd overcomplicated reduce.js in my first attempt. After starting over on this portion, it works fine.

--- reduce.js
+++ reduce.js
@@ -1,19 +1,12 @@
 //Phil Hofer
-//3/2/15
 //reduce.js
-//This program reduces the dataset to the champion with the lowest Attack.
+//3/2/15
+//This program returns the single champion with the lowest Attack.
+
 
 load("dataset.js");
 var league = JSON.parse(LEAGUE);
 
-function reduce(array, combine, start) {
-  var current = start;
-  for (var i = 0; i < array.length; i++)
-    current = combine(current, array[i]);
-  return current;
-}
-
-print(JSON.stringify(reduce(league, function(min, cur) {
-  if (cur.Attack < min.Attack) return cur;
-  else return min;
+print(JSON.stringify(league.reduce(function(min, cur) {
+  return (cur.Attack < min.Attack)? cur : min;
 })));
